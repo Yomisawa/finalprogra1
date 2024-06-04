f71407dd0beb946782e4f10b2f67ea50258c5c35
Yeferson lemus <evar827@gmail.com>
2024-06-04 00:39:52 -0600

    program2

--- WindowsFormsAppVaidrollTeam/Program.cs
+++ WindowsFormsAppVaidrollTeam/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppVaidrollTeam
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
