a82a91d8aa2cb36b229d07c667fe00849c58fe3f
Kevin.Orellana <korellanaa6@miumg.edu.gt>
2024-06-04 00:23:30 -0600

    Datos18

--- Capa_Datos/ClassDatos.cs
+++ Capa_Datos/ClassDatos.cs
@@ -26,7 +26,14 @@ namespace Capa_Datos
             return dt;
         }
 
-       
+        public DataTable D_listarusuarios()
+        {
+            SqlCommand cmd = new SqlCommand("sp_listar_usuarios", cn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
 
         public DataTable D_buscarusuarios(ClassEntidad obje)
         {
